16bee58bdbbdb4905a57d7bfac4cda24901439c1
JosephZhou19 <jyzhou3@wisc.edu>
2025-12-21 18:11:37 -0500

    added drawdown and probability of loss

--- app/routers/SimulationRouter.py
+++ app/routers/SimulationRouter.py
@@ -18,6 +18,6 @@ def simulate_basic_wealth(years: int):
     return {"years": years, "total_wealth": total, "asset_totals": asset_totals}
 @router.get("/advanced/{years}")
 def simulate_advanced_wealth(years: int):
-    wealth_service = WealthService(10)
+    wealth_service = WealthService(1000)
     paths = wealth_service.simulate_advanced_wealth(years)
     return {"years": years, "paths": paths}
--- app/service/WealthService.py
+++ app/service/WealthService.py
@@ -38,34 +38,64 @@ class WealthService:
         paths = []
         for i in range(self.MT_PATHS):
             paths.append(self.simulate_path(assets, asset_rule_map, years))
+        wealth_totals = [path[0] for path in paths]
+        p25 = np.percentile(wealth_totals, 25)
+        p75 = np.percentile(wealth_totals, 75)
+        p50 = np.percentile(wealth_totals, 50)
+        p5 = np.percentile(wealth_totals, 5)
+        p95 = np.percentile(wealth_totals, 95)
+        max_drawdown = max([path[1] for path in paths])
+        probability_of_loss = sum([path[2] for path in paths]) / len(paths)
         db.close()
-        return paths
+        return {
+            "p5": p5,
+            "p25": p25,
+            "p50": p50,
+            "p75": p75,
+            "p95": p95,
+            "max_drawdown": max_drawdown,
+            "probability_of_loss": probability_of_loss
+        }
     def simulate_path(self, assets, asset_rule_map, years):
         portfolio_total = 0.0
+        peak = sum([asset.initial_value for asset in assets])
+        max_drawdown = 0.0
         rng = np.random.default_rng()
-        for asset in assets:
-            asset_total = self.simulate_asset(asset, asset_rule_map[asset.name], years, rng)
-            portfolio_total += asset_total
-        return portfolio_total
+        no_growth_total = peak
+        isLoss = False
+        # Simulate monthly growth
+        for i in range(years * 12):
+            for asset in assets:
+                asset_total = self.simulate_asset_growth(asset, asset_rule_map[asset.name], rng)
+                portfolio_total += asset_total
+            # calculate draw down
+            peak = max(peak, portfolio_total)
+            drawdown = (peak - portfolio_total) / peak
+            max_dd = max(max_drawdown, drawdown)
+            for asset in assets:
+                asset_contribution = sum([rule.rate for rule in asset_rule_map[asset.name]])
+                portfolio_total += asset_contribution
+                no_growth_total += asset_contribution   
+        if no_growth_total > portfolio_total:
+            isLoss = True
+        return portfolio_total, max_dd, isLoss
     
-    def simulate_asset(self, asset, rules, years, rng: np.random.Generator):
+    def simulate_asset_growth(self, asset, rules, rng: np.random.Generator):
         dt = 1.0 / 12.0
         asset_total = asset.initial_value
         rate = asset.expected_return
-        # Simulate monthly growth
-        for i in range(years * 12):
-            z = rng.normal(0, 1)
-            rate_change = rng.normal(0, asset.return_volatility) * np.sqrt(dt)
-            rate += rate_change
-            rate = max(rate, 0.0)
-            logger.info(f"Rate: {rate}")
-            growth = np.exp((rate - 0.5 * asset.volatility ** 2) * dt + asset.volatility * np.sqrt(dt) * z)
-            new_value = asset_total * growth
-            gain = new_value - asset_total
-            if gain > 0:
-                gain = gain * (1 - asset.tax_drag) 
-            asset_total = asset_total + gain + sum([rule.rate for rule in rules])
-            logger.info(f"Asset: {asset.name}, Month: {i+1}, Value: {asset_total}")
+        z = rng.normal(0, 1)
+        rate_change = rng.normal(0, asset.return_volatility) * np.sqrt(dt)
+        rate += rate_change
+        rate = max(rate, 0.0)
+        logger.info(f"Rate: {rate}")
+        growth = np.exp((rate - 0.5 * asset.volatility ** 2) * dt + asset.volatility * np.sqrt(dt) * z)
+        new_value = asset_total * growth
+        gain = new_value - asset_total
+        if gain > 0:
+            gain = gain * (1 - asset.tax_drag) 
+        asset_total = asset_total + gain + sum([rule.rate for rule in rules])
+        logger.info(f"Asset: {asset.name}, Value: {asset_total}")
         return asset_total
 
 
